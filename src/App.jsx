77511331232fd33512705d772c1d75671f1d4ca2
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-01 13:30:37 +0330

    sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import SideBar from "./components/topbar/sidebar/SideBar";
 import Topbar from "./components/topbar/Topbar";
 import routes from "./routes";
 import { useRoutes } from "react-router-dom";
+import "./App.css";
 
 export default function App() {
   let router = useRoutes(routes);
   return (
     <>
       <Topbar />
-      {router}
+      <div className="container">
+        <SideBar />
+        {router}
+      </div>
     </>
   );
 }
