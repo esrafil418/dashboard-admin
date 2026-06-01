ecaa8deecec9ec83e82246da59bfdb4b82ad6064
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-01 12:25:05 +0330

    Topbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Topbar from "./components/topbar/Topbar";
 import routes from "./routes";
 import { useRoutes } from "react-router-dom";
 
 export default function App() {
   let router = useRoutes(routes);
-  return <>{router}</>;
+  return (
+    <>
+      <Topbar />
+      {router}
+    </>
+  );
 }
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,35 @@
+import "./Topbar.css";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import LanguageIcon from "@mui/icons-material/Language";
+import SettingsIcon from "@mui/icons-material/Settings";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Squid Game</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNoneIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <LanguageIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <SettingsIcon />
+          </div>
+
+          <img
+            src="images\Seong Gi-hun.webp"
+            alt="Seong Gi-hun"
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
