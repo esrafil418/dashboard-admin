844a17502356c2561b39d2d8c465db600c8bdbd1
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-01 16:07:48 +0330

    feat: Add Chart Feature

---
+++ src/datas.js
@@ -0,0 +1,52 @@
+let xAxisData = [
+  {
+    name: "Jan",
+    Sale: 100_030,
+  },
+  {
+    name: "Feb",
+    Sale: 50_040,
+  },
+  {
+    name: "Mar",
+    Sale: 100_160,
+  },
+  {
+    name: "Apr",
+    Sale: 90_120,
+  },
+  {
+    name: "May",
+    Sale: 120_800,
+  },
+  {
+    name: "Jun",
+    Sale: 180_300,
+  },
+  {
+    name: "Jul",
+    Sale: 60_090,
+  },
+  {
+    name: "Agu",
+    Sale: 140_020,
+  },
+  {
+    name: "Sep",
+    Sale: 80_440,
+  },
+  {
+    name: "Oct",
+    Sale: 50_000,
+  },
+  {
+    name: "Nov",
+    Sale: 110_000,
+  },
+  {
+    name: "Dec",
+    Sale: 130_000,
+  },
+];
+
+export { xAxisData };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,13 @@
+import Chart from "../../components/topbar/chart/Chart";
 import Features from "../../components/topbar/features/Features";
+import "./Home.css";
+import { xAxisData } from "../../datas";
 
 export default function Home() {
-  return <Features />;
+  return (
+    <div className="home">
+      <Features />
+      <Chart grid title={"Month Sale"} data={xAxisData} dataKey={"Sale"} />
+    </div>
+  );
 }
